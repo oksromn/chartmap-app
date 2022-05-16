22758a3539b22b95df4f8fd8d9ed031d4ab92242
Roman <advenua@gmail.com>
2022-05-16 21:37:11 +0300

    Added configuration for locales to config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module ChartmapApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{yml}')]
+
+    # Permitted locales available for the application
+    config.i18n.available_locales = [:ru, :en]
+
+    # Set default locale
+    config.i18n.default_locale = :ru
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
